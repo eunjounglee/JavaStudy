5efe5ee9ee49fadf1ccd6a2abd83159ab43b8151
eunjounglee <49137395+eunjounglee@users.noreply.github.com>
2019-04-09 17:58:39 +0900

    third

--- JavaStudy/src/ch05/ArrayExam3.java
+++ JavaStudy/src/ch05/ArrayExam3.java
@@ -5,7 +5,7 @@ public class ArrayExam3 {
 		 int[] numbers = {0, 3, 1, 7, 4};
 
 		 int min = Integer.MAX_VALUE;
-		 System.out.println(min);
+//		 System.out.println(min);
 
 		 for(int i = 0; i < numbers.length; i++) {
 		 if(numbers[i] < min) {
--- JavaStudy/src/ch05/CutOperation.java
+++ JavaStudy/src/ch05/CutOperation.java
@@ -1,5 +1,17 @@
 package ch05;
 
 public class CutOperation {
-
+	public static void main(String[] args) {
+		int[] stick = { 5, 4, 4, 2, 2, 8 };
+		
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < stick.length; i++) {
+			if (stick[i] < min) {
+				min = stick[i];
+			}
+		}
+		System.out.println(stick.length);
+		
+		
+	}
 }
