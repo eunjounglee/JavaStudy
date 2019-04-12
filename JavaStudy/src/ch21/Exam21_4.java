ae933f03c2e5d9aa31b3f2975266aa13bcf67ec7
eunjounglee <49137395+eunjounglee@users.noreply.github.com>
2019-04-12 21:06:31 +0900

    2019.04.12

--- JavaStudy/src/ch21/Exam21_4.java
+++ JavaStudy/src/ch21/Exam21_4.java
@@ -3,42 +3,58 @@ package ch21;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 import javax.swing.JButton;
-
+import javax.xml.crypto.Data;
 
 public class Exam21_4 {
 	public static void main(String[] args) {
-		 Bingo b = new Bingo();
-		 b.display();
-		 }
+		Bingo b = new Bingo();
+		b.display();
+	}
+}
+
+class BtnHandler implements ActionListener {
+	int[][] bingo = new int[5][5]; // 모든요소 0으로 초기화
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton btn = (JButton) e.getSource();
+		btn.setBackground(Color.LIGHT_GRAY);
+
+		// 선택된 버튼의 좌표 확인 (setName으로 지정된 값)
+		String xy = btn.getName();
+		System.out.println(xy);
+		// x y xharAt(0) x y 분리
+		char x = '0';
+		char y = '1';
+		int numX = Integer.parseInt(x + "");
+		int numY = Integer.parseInt(y + "");
+		bingo[numX][numY] = 1;
+
+		/* 가로줄 완성 확인 코드 */
+
+		TreeSet<Integer> treeSetX = new TreeSet<Integer>();
+		treeSetX.add(numX);
+//		Iterator<Integer> itrx = treeSetX.iterator();
+
+		
+		TreeSet<Integer> treeSetY = new TreeSet<Integer>();
+		treeSetY.add(numY);
+//		Iterator<Integer> itry = treeSetY.iterator();
+		
+		if(treeSetY.size() == 1) {
+			if(treeSetX.size() == 5) {
+				System.out.println("가로 줄 빙고!");
+			}
 		}
-		class BtnHandler implements ActionListener {
-			int[][]bingo = new int[5][5]; //모든요소 0으로 초기화
-		 @Override
-		 public void actionPerformed(ActionEvent e) {
-		 JButton btn = (JButton) e.getSource();
-		 btn.setBackground(Color.LIGHT_GRAY);
+		
 
-		 // 선택된 버튼의 좌표 확인 (setName으로 지정된 값)
-		 String xy = btn.getName();
-		 System.out.println(xy);
-		 //x y xharAt(0) x y 분리
-		 char x = '0';
-		 char y = '1';
-		 int numX = Integer.parseInt(x + "");
-		 int numY = Integer.parseInt(y + "");
-		 bingo[numX][numY] = 1;
-		 
-		 /* 가로줄 완성 확인 코드 */
 
-		 
-		 
-		 
+		/* 세로줄 완성 확인 코드 */
 
-		 /* 세로줄 완성 확인 코드 */
-		 
-		 
-		 }
+	}
 
 }
